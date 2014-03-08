d8047ee96bfc9d3c9108d15a3471905384f42f90
vytautas <vytautas@localhost.localdomain>
2014-03-09 00:05:31 +0200

    Make company copyright like variable

--- demo_app/demo/settings.py
+++ demo_app/demo/settings.py
@@ -131,6 +131,16 @@ TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, "templates"),
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    "xadmin.context_processors.admin_branding",
+)
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -148,6 +158,8 @@ INSTALLED_APPS = (
 DATE_FORMAT = 'Y-m-d'
 DATETIME_FORMAT = 'Y-m-d H:i'
 TIME_FORMAT = 'H:i'
+
+COPYRIGHT= u"\u00A9 my-company.inc 2014"
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
